7602fe2dbd52b22d44befee678c4b6b4fa3ce723
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-04-25 20:41:46 +0700

    Anti-Aliasing and Setting Overhauling

--- src/bluearchive/ArchiveDustry.java
+++ src/bluearchive/ArchiveDustry.java
@@ -2,36 +2,33 @@ package bluearchive;
 
 import arc.*;
 import arc.audio.*;
-import arc.graphics.*;
 import arc.graphics.Color;
+import arc.scene.style.Drawable;
 import arc.scene.style.TextureRegionDrawable;
-import arc.scene.ui.*;
 import arc.scene.ui.Image;
+import arc.scene.ui.ImageButton;
+import arc.scene.ui.TextArea;
+import arc.scene.ui.TextField;
 import arc.scene.ui.layout.*;
+import arc.scene.utils.Elem;
 import arc.util.*;
-import mindustry.Vars;
 import mindustry.gen.*;
-import mindustry.graphics.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import bluearchive.units.*;
-import mindustry.net.Net;
 import mindustry.ui.Links;
 import mindustry.ui.dialogs.*;
 import mindustry.ui.dialogs.SettingsMenuDialog.*;
 import mindustry.ui.dialogs.SettingsMenuDialog.SettingsTable.*;
-
-import java.awt.*;
 
 import static mindustry.Vars.*;
 
 public class ArchiveDustry extends Mod {
 
     public ArchiveDustry() {
-
         //listen for game load event
         Events.on(ClientLoadEvent.class, event -> {
-            Log.info("[AchivD] ArchiveDustry fully loaded!.");
+            Log.info("[ArchivD] ArchiveDustry fully loaded!.");
             if(Core.settings.getBool("ba-firstTime")) {
                 BaseDialog dialog = new BaseDialog(Core.bundle.get("ba-firstTimeDialog.title"));
                 dialog.cont.image(Core.atlas.find("bluearchive-mikalove")).pad(20).size(200f, 200f).row();
@@ -104,24 +101,105 @@ public class ArchiveDustry extends Mod {
             tree.loadMusic("win").play();
         });
     }
-
     @Override
     public void init(){
         if(Core.settings.getBool("ba-addHalo")) {
             UnitHalo.init();
         }
         loadSettings();
+        new Links.LinkEntry("ba-youtube", "https://www.youtube.com/channel/UCsrnDYrkovQhCCE8kwKcvKQ", Icon.play, Color.red);
+        Log.info("[ArchivD] Link Generated!");
+        if(Core.settings.getBool("Heart of Iron 4 Main Menu Soundtrack")){
+            Musics.menu = tree.loadMusic("menuhoi4");
+        } else {
+            Musics.menu = tree.loadMusic("menuba");
+        }
     }
 
     void loadSettings(){
         ui.settings.addCategory("ArchiveDustry", t -> {
             t.pref(new Banner("bluearchive-logo", -1));
+            t.pref(new TextSeparator(Core.bundle.get("setting.category.general-setting")));
+            t.pref(new Separator(4));
             t.checkPref("ba-firstTime", true);
             t.checkPref("ba-addHalo", true);
-            new Links.LinkEntry("ba-youtube", "https://www.youtube.com/channel/UCsrnDYrkovQhCCE8kwKcvKQ", Icon.play, Color.red);
+            t.pref(new TextSeparator(Core.bundle.get("setting.category.links")));
+            t.pref(new Separator(4));
+            t.pref(new ButtonSetting("ba-youtube", Icon.play, () -> {
+                Core.app.openURI("https://www.youtube.com/channel/UCsrnDYrkovQhCCE8kwKcvKQ");
+            }, 32));
+            t.pref(new ButtonSetting("ba-github", Icon.github, () -> {
+                Core.app.openURI("https://www.github.com/willoizcitron/archivedustry-java");
+            }, 32));
+            t.pref(new Separator(2));
+            if (OS.username.startsWith("willoizcitron")){
+                t.pref(new Separator(2));
+                t.pref(new TextSeparator("< Developer Settings >"));
+                t.checkPref("Heart of Iron 4 Main Menu Soundtrack", false, b -> {
+                if(b) {
+                    Musics.menu = tree.loadMusic("menuhoi4");
+                } else {
+                    Musics.menu = tree.loadMusic("menuba");
+                }
+                });
+            }
         });
     }
 
+    /** Not a setting, but rather a button in the settings menu. */
+    static class ButtonSetting extends Setting{
+        Drawable icon;
+        Runnable listener;
+        float iconSize;
+
+        public ButtonSetting(String name, Drawable icon, Runnable listener, float iconSize){
+            super(name);
+            this.icon = icon;
+            this.listener = listener;
+            this.iconSize = iconSize;
+        }
+
+        @Override
+        public void add(SettingsTable table){
+            ImageButton b = Elem.newImageButton(icon, listener);
+            b.resizeImage(iconSize);
+            b.label(() -> title).padLeft(6).growX();
+            b.left();
+
+            addDesc(table.add(b).left().padTop(3f).get());
+            table.row();
+        }
+    }
+
+static class TextSeparator extends Setting {
+        String text;
+        public TextSeparator(String text){
+            super("");
+            this.text = text;
+        }
+        @Override
+        public void add(SettingsTable table){
+            TextField b = new TextField(text);
+            b.setText(text);
+            b.setDisabled(true);
+            b.setAlignment(1);
+            table.add(b).growX();
+            table.row();
+        }
+}
+    static class Separator extends Setting{
+        float height;
+        public Separator(float height){
+            super("");
+            this.height = height;
+        }
+
+        @Override
+        public void add(SettingsTable table){
+            table.image(Tex.clear).height(height).padTop(3f);
+            table.row();
+        }
+    }
     /** Not a setting, but rather adds an image to the settings menu. */
     static class Banner extends Setting{
         float width;
