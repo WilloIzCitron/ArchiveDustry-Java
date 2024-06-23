e5899fecb69e76b0c2b115034e15c299c84662e0
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-06-23 18:13:39 +0700

    tweak

--- src/bluearchive/ui/dialogs/ArchivDLive2DManager.java
+++ src/bluearchive/ui/dialogs/ArchivDLive2DManager.java
@@ -16,9 +16,10 @@ import static arc.Core.*;
 public class ArchivDLive2DManager extends BaseDialog {
     Dialog restartDialog = new Dialog(){{
         cont.add(new Table(){{
-            cont.image(atlas.find("bluearchive-arona-happy")).size(256,256).row();
+            cont.image(atlas.find("bluearchive-arona-happy")).size(256,256).center().row();
             new Table(){{
-                    cont.add(bundle.get("ba-restartDialogText")).row();
+                    cont.add("Arona").left().row();
+                    cont.add(bundle.get("ba-restartDialogText")).right().row();
                     cont.button(bundle.get("ba-restartConfirm"), Styles.flatt, () -> Core.app.exit()).size(250f, 50f);
                 }};
         }}).style(Styles.grayi);
@@ -26,11 +27,10 @@ public class ArchivDLive2DManager extends BaseDialog {
     Table tabl = new Table(){{
         for (int i = 1; i < 7; i++) {
             int finalI = i;
-            new Group() {{
-                add(bundle.get("ba-l2d"+ finalI +".name")).row();
-                add(bundle.get("ba-l2d"+ finalI +".author")).color(Color.gray).row();
-                button("Apply", () -> { Core.settings.put("setL2D", finalI); restartDialog.show();}).growX().row();
-            }};
+            cont.button(con -> {
+                            con.add(bundle.get("ba-l2d"+ finalI +".name")).row();
+                            con.add(bundle.get("ba-l2d"+ finalI +".author")).color(Color.gray);
+                            }, Styles.flatBordert, () -> { Core.settings.put("setL2D", finalI); restartDialog.show();});
         }
     }};
 
