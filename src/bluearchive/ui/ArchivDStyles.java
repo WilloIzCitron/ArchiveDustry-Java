258237687ab9a6d9911aa7719256a98e1fefabe4
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2024-10-18 21:26:47 +0700

    Temporary commented

--- src/bluearchive/ui/ArchivDStyles.java
+++ src/bluearchive/ui/ArchivDStyles.java
@@ -1,47 +1,47 @@
-package bluearchive.ui;
-
-import arc.scene.Element;
-import mindustry.core.UI;
-import mindustry.ui.*;
-
-import arc.scene.style.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-
-public class ArchivDStyles extends Styles{
-
-    public static void load(){
-        var whiteui = (TextureRegionDrawable)Tex.whiteui;
-        accentDrawable = whiteui.tint(Pal.techBlue);
-
-        nonet.overFontColor = Pal.techBlue;
-        emptyi.imageDownColor = Pal.techBlue;
-        geni.imageDownColor = Pal.techBlue;
-        defaultDialog.titleFontColor = Pal.techBlue;
-        fullDialog.titleFontColor = Pal.techBlue;
-        techLabel.fontColor = Pal.techBlue;
-        defaultt.downFontColor = Pal.techBlue;
-        defaulti.imageDownColor = Pal.techBlue;
-        flatt.downFontColor = Pal.techBlue;
-        flati.imageDownColor = Pal.techBlue;
-        flatBordert.downFontColor = Pal.techBlue;
-        defaultCheck.checkedFontColor = Pal.techBlue;
-
-        defaultt.overFontColor = Pal.techBlue;
-        defaulti.imageOverColor = Pal.techBlue;
-        flatt.overFontColor = Pal.techBlue;
-        flatBordert.overFontColor = Pal.techBlue;
-        flati.imageOverColor = Pal.techBlue;
-        squarei.imageDownColor = Pal.techBlue;
-        squarei.imageOverColor = Pal.techBlue;
-        defaultt.checkedFontColor = Pal.techBlue;
-        defaulti.imageCheckedColor = Pal.techBlue;
-        flatt.checkedFontColor = Pal.techBlue;
-        flati.imageCheckedColor = Pal.techBlue;
-        squarei.imageCheckedColor = Pal.techBlue;
-        defaultt.checkedOverFontColor = Pal.techBlue;
-        flatt.checkedOverFontColor = Pal.techBlue;
-
-    }
-}
-
+//package bluearchive.ui;
+//
+//import arc.scene.Element;
+//import mindustry.core.UI;
+//import mindustry.ui.*;
+//
+//import arc.scene.style.*;
+//import mindustry.gen.*;
+//import mindustry.graphics.*;
+//
+//public class ArchivDStyles extends Styles{
+//
+//    public static void load(){
+//        var whiteui = (TextureRegionDrawable)Tex.whiteui;
+//        accentDrawable = whiteui.tint(Pal.techBlue);
+//
+//        nonet.overFontColor = Pal.techBlue;
+//        emptyi.imageDownColor = Pal.techBlue;
+//        geni.imageDownColor = Pal.techBlue;
+//        defaultDialog.titleFontColor = Pal.techBlue;
+//        fullDialog.titleFontColor = Pal.techBlue;
+//        techLabel.fontColor = Pal.techBlue;
+//        defaultt.downFontColor = Pal.techBlue;
+//        defaulti.imageDownColor = Pal.techBlue;
+//        flatt.downFontColor = Pal.techBlue;
+//        flati.imageDownColor = Pal.techBlue;
+//        flatBordert.downFontColor = Pal.techBlue;
+//        defaultCheck.checkedFontColor = Pal.techBlue;
+//
+//        defaultt.overFontColor = Pal.techBlue;
+//        defaulti.imageOverColor = Pal.techBlue;
+//        flatt.overFontColor = Pal.techBlue;
+//        flatBordert.overFontColor = Pal.techBlue;
+//        flati.imageOverColor = Pal.techBlue;
+//        squarei.imageDownColor = Pal.techBlue;
+//        squarei.imageOverColor = Pal.techBlue;
+//        defaultt.checkedFontColor = Pal.techBlue;
+//        defaulti.imageCheckedColor = Pal.techBlue;
+//        flatt.checkedFontColor = Pal.techBlue;
+//        flati.imageCheckedColor = Pal.techBlue;
+//        squarei.imageCheckedColor = Pal.techBlue;
+//        defaultt.checkedOverFontColor = Pal.techBlue;
+//        flatt.checkedOverFontColor = Pal.techBlue;
+//
+//    }
+//}
+//
